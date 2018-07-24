d0ed396a5d5e0dd1fa3adb2532fb357066028800
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-24 18:34:41 +0530

    added ScopedModel to WidgetTree

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_scoped_model/model/todos.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class TodosPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: _buildTodos(),
+    );
+  }
+
+  _buildTodos() {
+    return ScopedModel<Todos>(
+      model: Todos(),
+      child: Container(
+        color: Colors.yellow,
+      ),
+    );
   }
 }
