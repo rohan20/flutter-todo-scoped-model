6c01dccda980b874ffa2bc6a839c00c37b6a435a
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-24 18:36:26 +0530

    added calls to notifyListeners in TodosModel

--- lib/model/todos.dart
+++ lib/model/todos.dart
@@ -9,21 +9,25 @@ class Todos extends Model {
 
   void addTodo(ToDo todo, {int position = 0}) {
     _todosList.insert(position, todo);
+    notifyListeners();
   }
 
   void markTodoAsChecked(ToDo todo) {
     removeTodo(todo);
     _todosList.insert(_todosList.length, todo);
     _checkedTodos.add(todo);
+    notifyListeners();
   }
 
   void markTodoAsUnchecked(ToDo todo) {
     removeTodo(todo);
     _todosList.insert(0, todo);
     _checkedTodos.remove(todo);
+    notifyListeners();
   }
 
   void removeTodo(ToDo todo) {
     _todosList.remove(todo);
+    notifyListeners();
   }
 }
