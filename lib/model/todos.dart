d3c9beb057121bb235c65298064bb003fed4abda
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-24 18:29:17 +0530

    added Model class Todos

---
+++ lib/model/todos.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_todo_scoped_model/model/to_do.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class Todos extends Model {
+  List<ToDo> _todosList = [];
+  Set<ToDo> _checkedTodos = Set<ToDo>();
+
+  List<ToDo> get todos => _todosList;
+
+  void addTodo(ToDo todo, {int position = 0}) {
+    _todosList.insert(position, todo);
+  }
+
+  void markTodoAsChecked(ToDo todo) {
+    removeTodo(todo);
+    _todosList.insert(_todosList.length, todo);
+    _checkedTodos.add(todo);
+  }
+
+  void markTodoAsUnchecked(ToDo todo) {
+    removeTodo(todo);
+    _todosList.insert(0, todo);
+    _checkedTodos.remove(todo);
+  }
+
+  void removeTodo(ToDo todo) {
+    _todosList.remove(todo);
+  }
+}
