994872713fcc65f5291ef5c2f4dc07a081b15adb
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-24 18:31:37 +0530

    added TodosPage stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,10 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_scoped_model/pages/todos_page.dart';
+
+void main() {
+  runApp(
+    new MaterialApp(
+      home: TodosPage(),
+    ),
+  );
+}
---
+++ lib/pages/todos_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TodosPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
