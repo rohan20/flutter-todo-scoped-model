9b014bb2a64c675b4a472f56cef0aed8029d3390
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-07-25 12:34:25 +0530

    cleanup: separated todo item into a different class

--- lib/pages/todos_page.dart
+++ lib/pages/todos_page.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_scoped_model/model/to_do.dart';
 import 'package:flutter_todo_scoped_model/model/todos.dart';
+import 'package:flutter_todo_scoped_model/widget/todo_widget.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class TodosPage extends StatelessWidget {
@@ -44,38 +45,8 @@ class TodosPage extends StatelessWidget {
     return ListView.builder(
       itemCount: model.todosCount,
       itemBuilder: (context, index) {
-        return _buildTodoItem(model.todos[index]);
+        return TodoItem(model.todos[index], model);
       },
-    );
-  }
-
-  _buildTodoItem(ToDo todo) {
-    bool isChecked = model.isCheckedTodo(todo);
-
-    return Dismissible(
-      key: Key(todo.id.toString()),
-      background: Container(
-        color: Colors.red[300],
-      ),
-      direction: DismissDirection.endToStart,
-      onDismissed: (direction) => model.removeTodo(todo),
-      child: InkWell(
-        onTap: () {
-          model.toggleTodo(todo);
-        },
-        child: Container(
-          padding: const EdgeInsets.all(12.0),
-          color: isChecked ? Colors.green[100] : null,
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              _buildTodoItemText(todo),
-              _buildTodoItemIcon(todo),
-            ],
-          ),
-        ),
-      ),
     );
   }
 
@@ -139,27 +110,6 @@ class TodosPage extends StatelessWidget {
           textAlign: TextAlign.center,
         ),
       ),
-    );
-  }
-
-  _buildTodoItemText(ToDo todo) {
-    return Flexible(
-      fit: FlexFit.tight,
-      flex: 1,
-      child: Text(
-        todo.title,
-        style: Theme.of(context).textTheme.headline,
-        maxLines: 1,
-        overflow: TextOverflow.ellipsis,
-      ),
-    );
-  }
-
-  _buildTodoItemIcon(ToDo todo) {
-    return Icon(
-      model.isCheckedTodo(todo) ? Icons.done : null,
-      color: Colors.white,
-      size: 40.0,
     );
   }
 
---
+++ lib/widget/todo_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_scoped_model/model/to_do.dart';
+import 'package:flutter_todo_scoped_model/model/todos.dart';
+
+class TodoItem extends StatelessWidget {
+  BuildContext context;
+  final ToDo todo;
+  final Todos model;
+
+  TodoItem(this.todo, this.model);
+
+  @override
+  Widget build(BuildContext context) {
+    this.context = context;
+    return _buildTodoItem(todo);
+  }
+
+  _buildTodoItem(ToDo todo) {
+    bool isChecked = model.isCheckedTodo(todo);
+
+    return Dismissible(
+      key: Key(todo.id.toString()),
+      background: Container(
+        color: Colors.blue[300],
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) => model.removeTodo(todo),
+      child: InkWell(
+        onTap: () {
+          model.toggleTodo(todo);
+        },
+        child: Container(
+          padding: const EdgeInsets.all(12.0),
+          color: isChecked ? Colors.green[100] : null,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              _buildTodoItemText(todo),
+              _buildTodoItemIcon(todo),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _buildTodoItemText(ToDo todo) {
+    return Flexible(
+      fit: FlexFit.tight,
+      flex: 1,
+      child: Text(
+        todo.title,
+        style: Theme.of(context).textTheme.headline,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+      ),
+    );
+  }
+
+  _buildTodoItemIcon(ToDo todo) {
+    return Icon(
+      model.isCheckedTodo(todo) ? Icons.done : null,
+      color: Colors.white,
+      size: 40.0,
+    );
+  }
+}
